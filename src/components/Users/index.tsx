d22c330c1d2663582a2eb423c8b1442b080e255e
Pedro Celestino <mr.pedrosilveira@gmail.com>
2023-05-29 21:14:10 -0300

    add loading and empty table message

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -2,6 +2,7 @@
 
 import { User } from "@prisma/client";
 import { useEffect, useState } from "react";
+import SpinLoading from "../SpinLoading";
 
 async function getUsers() {
   const res = await fetch("/api/contact");
@@ -15,32 +16,50 @@ async function getUsers() {
 
 const Users = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getUsers().then(setUsers);
+    getUsers()
+      .then(setUsers)
+      .finally(() => setLoading(false));
   }, []);
 
   return (
-    <table className="table-auto">
-      <thead>
-        <tr>
-          <th className="px-4 py-2">ID</th>
-          <th className="px-4 py-2">Nome</th>
-          <th className="px-4 py-2">Email</th>
-          <th className="px-4 py-2">Telefone</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user) => (
-          <tr key={user.id}>
-            <td className="border px-4 py-2">{user.id}</td>
-            <td className="border px-4 py-2 whitespace-nowrap">{user.name}</td>
-            <td className="border px-4 py-2">{user.email}</td>
-            <td className="border px-4 py-2">{user.phone}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      {loading ? (
+        <SpinLoading />
+      ) : (
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">ID</th>
+              <th className="px-4 py-2">Nome</th>
+              <th className="px-4 py-2">Email</th>
+              <th className="px-4 py-2">Telefone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td className="border px-4 py-2">{user.id}</td>
+                <td className="border px-4 py-2 whitespace-nowrap">
+                  {user.name}
+                </td>
+                <td className="border px-4 py-2">{user.email}</td>
+                <td className="border px-4 py-2">{user.phone}</td>
+              </tr>
+            ))}
+            {users.length === 0 && (
+              <tr>
+                <td className="border px-4 py-2 text-center" colSpan={4}>
+                  Nenhum usuário encontrado
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      )}
+    </>
   );
 };
 
